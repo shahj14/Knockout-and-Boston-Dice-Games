6cb9fb1f4b71f40bafc609cfe577f5831817fd04
Jeet Shah <jeetshah4321@gmail.com>
2017-09-10 09:38:22 -0400

    Added Dice, Player, and DiceGame classes

---
+++ main.cpp
@@ -0,0 +1,179 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <stdlib.h>
+
+#define MAX_PLAYERS 10
+
+using namespace std;
+
+/*
+	Class representing an N-sided die
+*/
+class Dice{
+    private:
+        int numSides;
+
+    public:
+        // Perform a single roll of the die
+        Dice(int n){
+            numSides = n;
+        }
+        int roll() {
+            srand(time(0));
+            return random()%numSides + 1;
+        };
+        void setNumSides(int n){
+            numSides = n;
+        }
+};
+
+/*
+	Class representing a Game Player
+*/
+class Player{
+    private:
+        string name;
+        int score;
+        bool alive;
+    public:
+        Player(){
+            name = "";
+            score = 0;
+            alive = true;
+        }
+        Player(string n, int s){
+            name = n;
+            score = s;
+            alive = true;
+        }
+        // Getter : Player Name
+        string getName() {
+            return name;
+        }
+        // Getter : Score
+        int getScore() {
+            return score;
+        }
+        void setName(string n){
+            name = n;
+        }
+        void setScore(int s){
+            score = s;
+        }
+        // Increases the player's score by a specified value
+        void addToScore(int val){
+            score += val;
+        }
+        void setAlive(bool status){
+            alive = status;
+        }
+        bool getAlive(){
+            return alive;
+        }
+};
+
+/*
+	Abstract class DiceGame
+*/
+class DiceGame{
+
+    protected:
+        Player *players[MAX_PLAYERS];
+        Dice *dice;
+        int numPlayers, numDice;
+
+    public:
+        //	Pure virtual function
+        virtual void play() = 0;
+
+        void initPlayers(){
+            do {
+                cout << "Enter number of players: ";
+                cin >> numPlayers;
+            }while(numPlayers > 10);
+
+            for(int i = 0; i < numPlayers; i++){
+                int score;
+                string name;
+                cout << "Enter name of Player " << i+1 << ": ";
+                cin >> name;
+                players[i] = new Player(name,0);
+            }
+        }
+
+        void displayScores(){
+            for(int i = 0; i < numPlayers; i++){
+                cout << players[i]->getName() << " : " << players[i]->getScore() << endl;
+            }
+        }
+        void requestDice(){
+            int numSides;
+            cout << "How many dice? : ";
+            cin >> numDice;
+            cout << "Number of sides on dice? : ";
+            cin >> numSides;
+            dice = new Dice(numSides);
+        }
+
+};
+class Knockout : public DiceGame{
+    void play(){
+        initPlayers();
+        requestDice();
+        //displayScores();
+        do {
+            int knockoutSum = getSum();
+            cout << "Knockout Sum: " << knockoutSum << endl;
+            cout << "Alive Sum: " << countAlive() << endl;
+            displayScores();
+            //cout << getSum() << endl;
+            //cout << getSum() << endl;
+             for (int i = 0; i < numPlayers; i++) {
+                //players[i]->setScore(getSum());
+                if (getSum() == knockoutSum) {
+                    players[i]->setAlive(false);
+                }
+                if(players[i]->getAlive() == true){
+                    players[i]->addToScore(1);
+                }
+            }
+            //cout << countAlive();
+        }while(countAlive() > 1);
+        getWinner();
+        displayScores();
+        //cout << getWinner()->getName();// << " : " << getWinner()->getScore() << endl;
+    }
+    int getSum(){
+        int sum = 0;
+        for(int i = 0; i < numDice; i ++){
+            sum += dice->roll();
+        }
+        return sum;
+    }
+    int countAlive(){
+        int sum =0;
+        for(int i = 0; i < numPlayers; i++){
+            if(players[i]->getAlive())
+                sum += 1;
+        }
+        return sum;
+    }
+    Player* getWinner(){
+        if(countAlive() == 1){
+            for(int i = 0; i < numPlayers; i++){
+                if(players[i]->getAlive())
+                    //cout << players[i]->getName() << endl;
+                    //cout << players[i] -> getScore();
+                    return players[i];
+            }
+        }
+    }
+};
+
+int main() {
+    // Base class pointer
+    DiceGame *game = new Knockout();
+    game->play();
+}
